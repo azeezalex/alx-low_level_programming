e14e72d9cbb1f6be9c86522f68af1d95416808bc
azeezalex <azeez.alex@ymail.com>
2022-09-10 12:29:13 -0700

    Task3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <ctype.h>
 /**
- * main - 
+ * main - Entry point
  * Return: Always 0 (success)
  */
 int main(void)
 {
 char ch;
+
 for (ch = 'A' ; ch <= 'Z' ; ch++)
 {	
 putchar(tolower(ch));
