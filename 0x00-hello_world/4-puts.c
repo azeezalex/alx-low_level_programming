c4ba7226e70c0ca902878660b423fc83f1d1bdbf
azeezalex <azeez.alex@ymail.com>
2022-09-10 04:36:12 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -19,10 +19,9 @@ int main(void)
 
 	char str1[70];
 
-	strcpy(str1, "\"Programming is like building a multilingual puzzle");
+	strcpy(str1,"Programming is like building a multilingual puzzle");
 
 	puts(str1);
 
 	return (0);
-
 }
