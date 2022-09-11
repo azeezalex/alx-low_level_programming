5140aecf57e65d8a6d37f4e9c077b9e816c87ac0
azeezalex <azeez.alex@ymail.com>
2022-09-11 11:34:42 -0700

    Task 100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * Write a program that prints all possible different combinations of two digits
+ *  Return: Always 0.
+ */
+int main(void)
+{
+int N;
+
+int M;
+
+for (N = 0; N <= 9; N++)
+{
+for (M = 0; M <= 9; M++)
+{
+if ((N != M) && (N < M)){
+putchar((N % 10) + '0');
+putchar((M % 10) + '0');
+putchar(',');
+putchar(' ');
+continue;
+}
+}
+}
+putchar('\n');
+return (0);
+}
